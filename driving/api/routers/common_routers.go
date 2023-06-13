8b368c22b97f11e4c2e4c411b36b30412ced7104
Say my name <vuphamlethanh.public@gmail.com>
2023-06-13 12:46:52 +0700

    feat<routers>: create `CommonRouters`

---
+++ driving/api/routers/common_routers.go
@@ -0,0 +1,24 @@
+package routers
+
+import (
+	"github.com/cable_management/cable_be/app/usecases/commomcase"
+	"github.com/cable_management/cable_be/driving/api/controllers/commoncontr"
+	"github.com/cable_management/cable_be/driving/api/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+type CommonRouters struct {
+	authContr commoncontr.IAuthController
+}
+
+func NewCommonRouters(authContr commoncontr.IAuthController) *CommonRouters {
+	return &CommonRouters{authContr: authContr}
+}
+
+func (a CommonRouters) Register(router gin.IRouter) {
+
+	router.POST("/sign-in",
+		middlewares.ParseBody[commomcase.SignInRequest],
+		a.authContr.SignIn)
+
+}
